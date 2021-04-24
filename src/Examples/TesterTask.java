ed4e2be826cfd0ab7d8418474bc79c925ca76b1a
Chinna <Chinna@Susmi>
2021-04-24 13:11:11 +0530

    Suggested changes are done in task

--- src/Examples/TesterTask.java
+++ src/Examples/TesterTask.java
@@ -3,7 +3,7 @@ package Examples;
 public class TesterTask {
 	public static void main(String args[]){
 		System.out.println("Done by the tester");
-		
+		System.out.println("It is done, suggested by lead in comments");
 	}
 	
 }
